efa62983db4a4ea4aebf6c3110e3225be1689625
cmpm <cmpm@dmz.org.br>
2022-05-15 17:15:20 -0300

    WIP: Updated with new formated data working with d3 lib - needs to clean methods structure and names
    Signed-off-by: cmpm <cmpm@dmz.org.br>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sunburst from 'react-d3-zoomable-sunburst';
 import { Paper } from '@mui/material';
 import { Grid } from '@mui/material';
 import json from "./data";
+import * as d3 from "d3";
 
 const SHEET_API = 'https://opensheet.elk.sh/1hJsZNdIPiENQZYqi2P75RMJmYcCeUtHBC6UalZ-fjE8/Tools+API';
 
@@ -18,7 +19,58 @@ function fetchData() {
     });
 }
 
+function testD3Group(data){
+  const grouped = d3.groups(
+    data, d => d.Area,
+    d => d.Method,
+    d => d.Algorithm,
+    d => d.Tools);
+  const toolsCount = d3.rollup(data, v => v.length, d => d.Algorithm);
+    // d => {return {"name": d.Tools, "Count": parseInt(d.Count)}});
+  console.log("grouped %o", grouped);
+  console.log("toolsCount %o", toolsCount);
+
+  const myHierarchy = d3.hierarchy(grouped);
+  console.log("myHierarchy %o", myHierarchy);
+
+  const mapTest = grouped.map(areaItem => {
+    return {
+      "name": areaItem[0],
+      "children": areaItem[1].map(methodItem => {
+        return {
+          "name": methodItem[0],
+          "children": methodItem[1].map(algorithmItem => {
+            return {
+              "name": algorithmItem[0],
+              "children": algorithmItem[1].map(toolItem => {
+                return {
+                  "name": toolItem[0],
+                  "Count": parseInt(toolItem[1][0].Count)
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
+  console.log("mapTest %o", mapTest);
+
+  return {"name":"Ferramentas de PCG","children": mapTest};
+}
+
 function formatData(data) {
+  const setArea = [...new Set(data.map(d => d.Area))];
+  const setTest = setArea.map(areaValue => {
+    return {
+      "name": areaValue,
+      "children": data.filter(valueB => {
+        d3.group(data, d => d.Area)
+      })
+    }
+  });
+  // console.log("setTEst %o", setTest);
+
   const childrenData = [...new Set(data.map(d => d.Area))].map(Area => {
     return {
       "name": Area,
@@ -40,23 +92,29 @@ function formatData(data) {
       })
     }
   });
-  console.log("childrenData")
-  console.log(childrenData)
+  // console.log("childrenData")
+  // console.log(childrenData)
   return { "name": "Ferramentas de PCG", "children": childrenData };
 }
 
 function App() {
-  const [data, setData] = useState({
-    data: {},
-    criteria: "Count",
-  });
+  const [data, setData] = useState({});
   const [apiData, setApiData] = useState({});
 
   useEffect(() => {
-    setApiData(fetchData().then(res => { formatData(res) }))
-    console.log("apiData")
-    console.log(apiData)
-    setData({...data, data: apiData});
+    const asyncFunction = async () => {
+      const res = await fetchData();
+      // console.log("fetchdata %o", res)
+      
+      // testD3Group(res);
+      // const formatedData = formatData(res);
+      const formatedData = testD3Group(res);
+      // setApiData(formatedData)
+      console.log("formatedData %o", formatedData)
+      setData(formatedData);
+    }
+    asyncFunction();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -69,14 +127,14 @@ function App() {
       <Grid item xs={12} md={12} lg={12}>
           <Paper elevation={3}>
             <Sunburst
-                data={data.data}
+                data={data}
                 scale="exponential"
                 tooltipContent={ <div class="sunburstTooltip" style="position:absolute; color:'black'; z-index:10; background: #e2e2e2; text-align: center; pointer-events: none;" /> }
                 tooltip
                 tooltipPosition="right"
                 keyId="Sunburst"
                 width={window.innerWidth * 0.8}
-                value={data.criteria}
+                value={"Count"}
                 height={window.innerHeight * 0.8}
             />
           </Paper>
