dade8e4a9690ef1dccfe20f90472d363471ccee5
cmpm <cmpm@dmz.org.br>
2022-05-17 22:09:31 -0300

    Adjusts to show data from tools
    Signed-off-by: cmpm <cmpm@dmz.org.br>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
 import Sunburst from 'react-d3-zoomable-sunburst';
-import { Paper } from '@mui/material';
-import { Grid } from '@mui/material';
-// import json from "./data";
+import { Grid, Card } from '@mui/material';
 import * as d3 from "d3";
 
 const SHEET_API = 'https://opensheet.elk.sh/1hJsZNdIPiENQZYqi2P75RMJmYcCeUtHBC6UalZ-fjE8/Tools+API';
@@ -19,109 +17,93 @@ function fetchData() {
     });
 }
 
-function testD3Group(data) {
-  const grouped = d3.groups(data, d => d.Area, d => d.Method, d => d.Algorithm, d => d.Tools);
-  const toolsCount = d3.rollup(data, v => v.length, d => d.Algorithm);
-  // d => {return {"name": d.Tools, "Count": parseInt(d.Count)}});
-  console.log("grouped %o", grouped);
-  console.log("toolsCount %o", toolsCount);
+function formatDataAsD3Group(data) {
+  const groupedData = d3.groups(data, d => d.Area, d => d.Method, d => d.Algorithm, d => d.Tools);
+  const algorithmsCount = d3.rollups(data, v => v.length, d => d.Algorithm);
 
-  const myHierarchy = d3.hierarchy(grouped);
-  console.log("myHierarchy %o", myHierarchy);
+  console.log("groupedData %o", groupedData);
+  console.log("algorithmsCount %o", algorithmsCount);
 
-  const mapTest = grouped.map(areaItem => {
+  const groupedDataMap = groupedData.map(areaItem => {
     return {
       "name": areaItem[0],
       "children": areaItem[1].map(methodItem => {
         return {
           "name": methodItem[0],
           "children": methodItem[1].map(algorithmItem => {
+            const foundAlgorithm = algorithmsCount.find(element => element[0] == algorithmItem[0]);
             return {
               "name": algorithmItem[0],
-              "children": algorithmItem[1].map(toolItem => {
-                return {
-                  "name": toolItem[0],
-                  "Count": parseInt(toolItem[1][0].Count)
-                }
-              })
+              "Count": foundAlgorithm[1],
+              "toolsList": algorithmItem[1]
             }
           })
         }
       })
     }
   })
-  console.log("mapTest %o", mapTest);
+  console.log("groupedDataMap %o", groupedDataMap);
 
-  return { "name": "Ferramentas de PCG", "children": mapTest };
+  return { "name": "Ferramentas de PCG", "children": groupedDataMap };
 }
-
-// function formatData(data) {
-//   const setArea = [...new Set(data.map(d => d.Area))];
-//   const setTest = setArea.map(areaValue => {
-//     return {
-//       "name": areaValue,
-//       "children": data.filter(valueB => {
-//         d3.group(data, d => d.Area)
-//       })
-//     }
-//   });
-//   // console.log("setTEst %o", setTest);
-
-//   const childrenData = [...new Set(data.map(d => d.Area))].map(Area => {
-//     return {
-//       "name": Area,
-//       "children": data.filter(d => d.Area === Area).map(d => {
-//         return {
-//           "name": d.Method,
-//           "children": data.filter(a => a.Method === d.Method).map(() => {
-//             return {
-//               "name": d.Algorithm,
-//               "children": data.filter(b => b.Algorithm === d.Algorithm).map(() => {
-//                 return {
-//                   "name": d.Tools,
-//                   "Count": parseInt(d.Count)
-//                 }
-//               })
-//             }
-//           })
-//         }
-//       })
-//     }
-//   });
-//   // console.log("childrenData")
-//   // console.log(childrenData)
-//   return { "name": "Ferramentas de PCG", "children": childrenData };
-// }
 
 function App() {
   const [data, setData] = useState({});
-  // const [apiData, setApiData] = useState({});
-
+  const [toolsList, setToolsList] = useState([]);
+  
   const tooltopStyle = {
-    position:'absolute',
-    color:'black',
-    'z-index':10,
+    position: 'absolute',
+    color: 'black',
+    'z-index': 10,
     background: '#e2e2e2',
     'text-align': 'center',
     'pointer-events': 'none'
   };
+  const cardStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    'text-align': 'left', 
+    padding: '10px',
+    margin: '5px',
+    width: '600px'
+  }
+  const cardDivStyle = { 
+    display: 'flex', 
+    flexDirection: 'row'
+  }
+
+  const onGraphClick = (event) => {
+    console.log("CLICK %o", event)
+  
+    if (event.parent !== null) { // not root
+      if (event.children?.length) {
+        event.children.forEach(element => {
+          if (element.children) {
+            element.children.forEach(subelement => {
+              setToolsList(subelement.data?.toolsList);
+              return;
+            })
+          }
+          setToolsList(element.data?.toolsList);
+          return;
+        });
+      }
+      setToolsList(event.data.toolsList)
+      return;
+    }
+  }
 
   useEffect(() => {
     const asyncFunction = async () => {
       const res = await fetchData();
-      // console.log("fetchdata %o", res)
-
-      // testD3Group(res);
-      // const formatedData = formatData(res);
-      const formatedData = testD3Group(res);
-      // setApiData(formatedData)
+      const formatedData = formatDataAsD3Group(res);
       console.log("formatedData %o", formatedData)
       setData(formatedData);
     }
     asyncFunction();
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
 
   // position: absolute;
   // top: 0px;
@@ -130,7 +112,6 @@ function App() {
   return (
     <div className="App">
       <Grid item xs={12} md={12} lg={12}>
-        <Paper elevation={3}>
           <Sunburst
             data={data}
             scale="exponential"
@@ -140,9 +121,41 @@ function App() {
             keyId="Sunburst"
             width={window.innerWidth * 0.8}
             value={"Count"}
-            height={window.innerHeight * 0.8}
+            height={window.innerHeight * 0.5}
+            onSelect={onGraphClick}
           />
-        </Paper>
+          <div style={cardDivStyle}>
+              {toolsList?.map((element) => {
+                var subelement = element[1][0];
+                return (
+                <Card variant="outlined" sx={cardStyle} key={element[0]}>
+                  <span>
+                    Nome da ferramenta: {subelement.Tools}
+                  </span>
+                  <span>
+                    <a href={subelement.Website}>Site</a> da ferramenta
+                  </span>
+                  <span>
+                    Github Stars: {subelement["Github Stars"]}
+                  </span>
+                  <span>
+                    Descrição: {subelement.Description}
+                  </span>
+                  <span>
+                    Sistema aplicável: {subelement["Engines/Systems"]}
+                  </span>
+                  <span>
+                    Área de PCG: {subelement.Area}
+                  </span>
+                  <span>
+                    Método de PCG: {subelement.Method}
+                  </span>
+                  <span>
+                    Algoritmo de PCG: {subelement.Algorithm}
+                  </span>
+                </Card>) 
+              })}
+          </div>
       </Grid>
     </div>
   );
